b51a2300b1f187e9e33b4228ee3bf153ec07e63e
paulTurchan <PaulTurchan@ip-192-168-10-7.ec2.internal>
2017-04-05 08:45:29 -0700

    Arranging schools

--- scrap.r
+++ scrap.r
@@ -2,11 +2,12 @@ library(rvest)
 library(dplyr)
 require(stringr)
 
-#Scrap Association of American Universities
+#Association of American Universities
 members <- data.frame(
   School = read_html("https://www.aau.edu/about/default.aspx?id=16710") %>%
     html_nodes("#ctl00_secaboutaauwidecph_main a") %>%
     html_text()
   ) %>% 
   filter(School != '') %>% #Drop blank td elements %>% 
-  mutate(School = str_trim(str_extract(School, "^[^(]+")))
+  mutate(School = str_trim(str_extract(School, "^[^(]+"))) %>% 
+  arrange(School)
